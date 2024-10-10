d1c25413d0894d8f4f2e80c8dda1d4c080e47990
rnguyen144 <115033118+rnguyen144@users.noreply.github.com>
2024-10-10 16:54:26 -0700

    It be working now Yay

---
+++ Vitis/ECE153a_Lab1/ECE153a_Lab1/platform.tcl
@@ -0,0 +1,23 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct C:\Users\ryand\OneDrive\Documents\Xilinx\ECE153A\Vitis\ECE153a_Lab1\ECE153a_Lab1\platform.tcl
+# 
+# OR launch xsct and run below command.
+# source C:\Users\ryand\OneDrive\Documents\Xilinx\ECE153A\Vitis\ECE153a_Lab1\ECE153a_Lab1\platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {ECE153a_Lab1}\
+-hw {C:\Users\ryand\OneDrive\Documents\Xilinx\ECE153A\Vivado\ECE_152A_Lab1\system_wrapper.xsa}\
+-proc {microblaze_0} -os {standalone} -out {C:/Users/ryand/OneDrive/Documents/Xilinx/ECE153A/Vitis/ECE153a_Lab1}
+
+platform write
+platform generate -domains 
+platform active {ECE153a_Lab1}
+bsp reload
+bsp setdriver -ip sevenSeg_0 -driver generic -ver 3.0 3.1
+bsp write
+bsp reload
+catch {bsp regenerate}
+platform generate
--- Vitis/Lab1/platform_Lab1/microblaze_0/standalone_microblaze_0/bsp/libsrc/build_configs/gen_bsp/CMakeFiles/tmrctr.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/tmrctr"
-  "xtmrctr_g.c"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/tmrctr.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
